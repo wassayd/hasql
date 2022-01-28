a48059db89be2a46715dca09bb6b8153967fec4f
wassayd <saidwaskar@gmail.com>
2022-01-28 23:32:27 +0100

    create author type

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
-module Main where
+{-# language BlockArguments #-}
+{-# language DeriveAnyClass #-}
+{-# language DeriveGeneric #-}
+{-# language DerivingStrategies #-}
+{-# language DerivingVia #-}
+{-# language DuplicateRecordFields #-}
+{-# language GeneralizedNewtypeDeriving #-}
+{-# language OverloadedStrings #-}
+{-# language StandaloneDeriving #-}
+{-# language TypeApplications #-}
+{-# language TypeFamilies #-}
+
+import Prelude
+import Rel8
 
 main :: IO ()
 main = putStrLn "test"
+
+data Author f = Author
+  { authorId :: Column f Int64
+  , name     :: Column f Text
+  , url      :: Column f (Maybe Text)
+  }
+  deriving stock (Generic)
+  deriving anyclass (Rel8able)
