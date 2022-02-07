5517694c86e8cc18015fdda37d599d940b872ae6
wassayd <saidwaskar@gmail.com>
2022-02-07 21:53:38 +0100

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@
 {-# language StandaloneDeriving #-}
 {-# language TypeApplications #-}
 {-# language TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
 
 import Prelude
 import Rel8
@@ -16,9 +17,30 @@ import Data.Text
 import GHC.Generics
 import Data.Int
 import Hasql.Connection
+import Hasql.Statement (Statement)
+import Hasql.Transaction (Transaction)
+import Control.Monad.IO.Class
+import qualified Control.Exception as Servant
+import Hasql.Pool
+import Control.Monad.Reader
 
 newtype AuthorId = AuthorId { toInt64 :: Int64 }
   deriving newtype (DBEq, DBType, Eq, Show)
+
+newtype AppEnv = AppEnv
+    { envDbPool :: Pool
+    }
+
+type AppM = AppEnv
+
+class HasDbPool env where
+  getDbPool :: env -> Pool
+
+runHandler env app = Servant.Handler $ ExceptT $ try $ runRIO env app
+
+instance HasDbPool AppEnv where
+    getDbPool = envDbPool
+
 
 
 data Author f = Author
@@ -61,11 +83,17 @@ projectSchema = TableSchema
   }
 
 
-connect :: IO Connection
-connect = do  
-  Right conn <- acquire $ settings "localhost" 5432 "postgres" "root" "postgres"
-  return conn
 
+runStmt :: Statement () a -> Transaction a
+runStmt = statement ()
+
+
+runTransaction :: forall a m env . (HasDbPool env, MonadReader env m, MonadIO m) => Transaction a -> m a
+runTransaction transaction = do
+    pool <- getDbPool
+    runTransactionWithPool pool transaction
+    
+executeStmt = runTransaction . runStmt
 
 getAllProject :: Query (Project Expr)
 getAllProject = each projectSchema
