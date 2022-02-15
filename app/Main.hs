680d8ca7bfacd4f3974783acbd05c3352403230d
wassayd <saidwaskar@gmail.com>
2022-02-15 22:56:56 +0100

    add getAllAuthors in main

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,10 @@ module Main where
 import ProjectRepository (getAllProject)
 import Rel8
 import Database (runqry)
+import AuthorRepository (getAllAuthor)
 
  
 main = do
   runqry "Get All Projects :" getAllProject
+  runqry "Get All Authors :" getAllAuthor
   putStrLn "Finished"
