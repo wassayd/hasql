f6530ad722c7059582d5a5234aefab27e383ed01
wassayd <saidwaskar@gmail.com>
2022-02-17 22:32:42 +0100

    wip add actions

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,35 @@ import ProjectRepository (getAllProject)
 import Rel8
 import Database (runqry)
 import AuthorRepository (getAllAuthor)
+import Classes
 
+data Action f = Action{
+    nameAction :: String
+  , fun :: f
+}
  
+availableAction :: Rel8able a => [Action (Query (a Expr))]
+availableAction = [ 
+  Action { nameAction="createPrj", fun = getAllProject }
+  ,Action { nameAction="s", fun = getAllAuthor  } ]
+  
+
+isActionAuthorized :: String -> Bool 
+isActionAuthorized "show authors"    = True   
+isActionAuthorized "show projects"   = True
+isActionAuthorized "create author"   = True   
+isActionAuthorized "create project"  = True
+isActionAuthorized "update author"   = True 
+isActionAuthorized "update project"  = True
+isActionAuthorized "delete author"   = True 
+isActionAuthorized "delete project"  = True
+isActionAuthorized _                 = False    
+
 main :: IO ()
 main = do
-  runqry "Get All Projects :" getAllProject
-  runqry "Get All Authors :" getAllAuthor
-  putStrLn "Finished"
+  putStr "Agrgument : "
+  action <- getLine
+  if isActionAuthorized action then 
+    putStrLn "Argument valid"
+  else
+    putStrLn "Argument invalid"
